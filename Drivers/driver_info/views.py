a625d221da2f6a47c477984cd9f198ffe33b4037
Dmitry Markevich <markevich3214@tut.by>
2023-08-15 20:53:52 +0300

    Update count time

--- Drivers/driver_info/views.py
+++ Drivers/driver_info/views.py
@@ -21,10 +21,10 @@ class DriverAPIView(APIView):
         queryset = list(DriverLog.objects.values('create_date', 'driver_id', 'status').filter(driver_id=driver_id))
         id = driver_id
         data = {}
-        for s in ('s', 'f', 'o'):
-            data[s] = round(sum([(queryset[(queryset.index(q) + 1)]['create_date'] - q['create_date']).total_seconds()
+        for state, t in zip(('s', 'f', 'o'), ('working_hours', 'time_relax', 'time_off')):
+            data[t] = round(sum([(queryset[(queryset.index(q) + 1)]['create_date'] - q['create_date']).total_seconds()
                                  if queryset.index(q) < len(queryset) - 1 else (
                     datetime.now(pytz.utc) - q['create_date']).total_seconds()
-                                 for q in queryset if q['status'] == s]) / 3600, 2)
+                                 for q in queryset if q['status'] == state]) / 3600, 2)
 
         return Response({'driver_id': driver_id, **data, })
